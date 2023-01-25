339629b797c6015f61c0cb51535afadad642e816
chinmay <chinmay29.shende@gmail.com>
2023-01-25 19:53:43 +0530

    Updated Insertion Class...

--- src/com/velocity/miniproject/InsertData.java
+++ src/com/velocity/miniproject/InsertData.java
@@ -6,12 +6,24 @@ import java.sql.PreparedStatement;
 public class InsertData {
 	PreparedStatement ps = null;
 	Connection con = null;
-	public void insertStudentData(int roll_no, String firstName,String middleName,String lastName,long mobileNo,String city ) {
-		
+	public void insertStudentData(int rollNo, String firstName,String middleName,String lastName,long mobileNo,String city,String password ) {
+	try {
+		ConnectionProvider cp = new ConnectionProvider();
+		con = cp.getConnectionDetails();
+		ps = con.prepareStatement("insert into quiz(rollNo,firstName,middleName,lastName,mobileNo,city,password)values(?,?,?,?,?,?,?)");
+		ps.setInt(1,rollNo);
+		ps.setString(2,firstName);
+		ps.setString(3,middleName);
+		ps.setString(4,lastName);
+		ps.setLong(5,mobileNo);
+		ps.setString(6,city);
+		ps.setString(7,password);
+		int i = ps.executeUpdate();
+		System.out.println("Record inserted Successfully..."+i);
+	} catch (Exception e) {
+		e.printStackTrace();
+	}	
 	}
-
-
-
 
 	public static void main(String[] args) {
 	
